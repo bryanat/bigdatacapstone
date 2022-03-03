aa531028ccc0ad64381388bbdb3201cbe2332fef
HamingnottT <Jnottingham836@yahoo.com>
2022-03-03 15:20:47 -0600

    Update branch with current docs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -22,9 +22,23 @@ import contextpack._
 object Main {
   def main(args: Array[String]) = {
     println("Main app started")
+    //System.setProperty("hadoop.home.dir", "C://hadoop")
+    
+    // Kafka main
+    //sMainKafka.startMainKafka()
+    //SparkStreamingContext.startSparkStreamingContext()
+    
+    // Producer main
+    //MainProducer.startMainProducer()
 
-    MainKafka.startMainKafka()
-    MainProducer.startMainProducer()
-    MainConsumer.startMainConsumer()
+    //MainKafka.startMainKafka()
+    //MainProducer.startMainProducer()
+    //MainConsumer.startMainConsumer()
+    //TestProducer.setProducer()
+    StructRead.subscribe()
+    StructWrite.send("topic1", Array("product_id", "product_category"))
+
+    //SparkStreamingContext.startSparkStreamingContext()
+
   }
 }
