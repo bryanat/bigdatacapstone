0e8153c783e59796004c90e30ee1def31bee7dfe
yueqi <yueqipeng2021@gmail.com>
2022-03-02 23:35:53 -0600

    add producer setup

---
+++ src/main/scala/kafkapack/TestConsumer.scala
@@ -0,0 +1,9 @@
+package kafkapack
+
+import scala.collection.JavaConverters._
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.StringDeserializer 
+
+object TestConsumer {
+  
+}
