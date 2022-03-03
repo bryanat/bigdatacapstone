7b8c1080e1069a81f00c477e4a3a79b405beb5e4
Yash D <60755759+dhayalytcnj@users.noreply.github.com>
2022-03-03 14:48:10 -0500

    testing queries with spark, but...
    spark causing errors. gonna test it in an old project with a stable spark

--- src/main/scala/consumerpack/YashConsumer.scala
+++ src/main/scala/consumerpack/YashConsumer.scala
@@ -5,8 +5,11 @@ import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.common.serialization._
 import java.util.Properties
 
+import org.apache.spark.sql._
+
 object YashConsumer {
     def main(args: Array[String]) = {
+        /*
         val props = new Properties()
         props.put("bootstrap.servers", "SERVER DETAILS HERE")
         props.put("group.id", "CONSUMER GROUP NAME")
@@ -19,10 +22,22 @@ object YashConsumer {
         while (true) {
             val result = kafkaConsumer.poll(2000).asScala
             for ((topic, data) <- result) {
-                /*
-                OPERATIONS THAT WILL OCCUR WHEN STREAMING DATA
-                 */
+                //OPERATIONS THAT WILL OCCUR WHEN STREAMING DATA
             }
         }
+        */
+
+        System.setProperty("hadoop.home.dir", "c:/winutils")
+        val spark = SparkSession
+          .builder()
+          .appName("bigdatacapstone")
+          .config("spark.master", "local")
+          .enableHiveSupport()
+          .getOrCreate()
+
+        val df = spark.read.csv("dataset-online/samplecsv.csv")
+        df.show();
+
+
     }
 }
