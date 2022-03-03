d037112977aeadd734d1a13d3062c976fe66db03
Yash D <60755759+dhayalytcnj@users.noreply.github.com>
2022-03-03 13:03:26 -0500

    Create YashConsumer.scala

---
+++ src/main/scala/consumerpack/YashConsumer.scala
@@ -0,0 +1,28 @@
+package consumerpack
+
+import scala.collection.JavaConverters._
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization._
+import java.util.Properties
+
+object YashConsumer {
+    def main(args: Array[String]) = {
+        val props = new Properties()
+        props.put("bootstrap.servers", "SERVER DETAILS HERE")
+        props.put("group.id", "CONSUMER GROUP NAME")
+        props.put("key.deserializer", classOf[StringDeserializer])
+        props.put("value.deserializer", classOf[StringDeserializer])
+
+        val kafkaConsumer = new KafkaConsumer[String, String](props)
+        kafkaConsumer.subscribe(COLLECTION_OF_TOPICS)
+
+        while (true) {
+            val result = kafkaConsumer.poll(2000).asScala
+            for ((topic, data) <- result) {
+                /*
+                OPERATIONS THAT WILL OCCUR WHEN STREAMING DATA
+                 */
+            }
+        }
+    }
+}
