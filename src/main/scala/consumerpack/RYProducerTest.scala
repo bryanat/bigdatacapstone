6e8607f83a328473ab2fcc1f1b06ac9f5d33d4e6
Hyung Ro (Ryan) Yoon <ryanyoon.gamedev@gmail.com>
2022-03-03 15:46:12 -0500

    committing test queries. sparkSession NOT WORKING

---
+++ src/main/scala/consumerpack/RYProducerTest.scala
@@ -0,0 +1,41 @@
+package consumerpack
+
+import java.util.Properties
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
+import org.apache.kafka.common.serialization.{IntegerSerializer, StringSerializer}
+
+class RYProducerTest {
+
+  val topicName = "test-2"
+
+  def producerTest(): Unit = {
+
+    val topicName = "test_2"
+
+    val producerProperties = new Properties()
+    producerProperties.setProperty(
+      ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"
+    )
+    producerProperties.setProperty(
+      ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[IntegerSerializer].getName
+    )
+    producerProperties.setProperty(
+      ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName
+    )
+
+    val producer = new KafkaProducer[Int, String](producerProperties)
+
+    producer.send(new ProducerRecord[Int, String](topicName, 10, "10,S1"))
+    producer.send(new ProducerRecord[Int, String](topicName, 20, "20,S2"))
+    producer.send(new ProducerRecord[Int, String](topicName, 30, "30,S3"))
+    producer.send(new ProducerRecord[Int, String](topicName, 40, "40,S4"))
+    producer.send(new ProducerRecord[Int, String](topicName, 50, "50,S5"))
+    producer.send(new ProducerRecord[Int, String](topicName, 60, "60,S6"))
+
+    producer.flush()
+
+    def customPartitioned(key: Int): Int = key % 3
+  }
+
+  def customPartitioned(key: Int): Int = key % 3
+}
