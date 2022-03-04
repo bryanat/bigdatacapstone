a330bfa9f84419c4095474ccd1443fa8c0c36a86
cameronlimRev <cameron.lim@revature.net>
2022-03-04 14:56:40 -0800

    More Test Functions to make Trends easier.
    - Added MaxPrice function to find the max price within our products
    
    - Added a filtering function to find high end products (WORKING BUT NOT DYNAMIC YET!)
    
    - Working on RandomSelections for trend work.
    
    - Removed category names since it was a pain to work with and unnecessary (can change if we disagree)

---
+++ src/main/scala/producerpack/RandomSelections.scala
@@ -0,0 +1,15 @@
+//package producerpack
+//
+//import java.util.Random
+//
+//object RandomSelections {
+//
+//  val rs = new Random()
+//
+//  def getRandomCustomerID(tag: String): String ={
+//    val orderNumber = rs.
+//  }
+// getRandomWebsite
+// getRandomProducty
+// getRandomCategory
+//}
