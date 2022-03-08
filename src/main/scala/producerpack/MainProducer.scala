5a4745b6621dbcc92f3ff0b26dee2c68595d0bc8
cameronlimRev <cameron.lim@revature.net>
2022-03-08 14:25:07 -0800

    Updated to use SparkSession from MainProducer

--- src/main/scala/producerpack/MainProducer.scala
+++ src/main/scala/producerpack/MainProducer.scala
@@ -4,23 +4,25 @@ import org.apache.spark.sql.SparkSession
 
 object MainProducer {
 
+    System.setProperty("hadoop.home.dir", "c:/winutils")
+    val spark = SparkSession
+      .builder()
+      .appName("project1")
+      .config("spark.master", "local")
+      .enableHiveSupport()
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
     def startMainProducer(): Unit = {
-        System.setProperty("hadoop.home.dir", "c:/winutils")
-        val spark = SparkSession
-          .builder()
-          .appName("project1")
-          .config("spark.master", "local")
-          .enableHiveSupport()
-          .getOrCreate()
-        spark.sparkContext.setLogLevel("ERROR")
 
         val trans = new Transactions
         val rs = new RandomSelections
 
         println("Main Producer started...")
-        var trend1Vector = trend1.getTrend1(100)
+        var trend1Vector = trend1.getTrend1(spark, 100)
         trend1Vector.foreach(println)
         var randomVector = trans.getRandomTransactions(rs, spark, 500)
         randomVector.foreach(println)
     }
+
 }
