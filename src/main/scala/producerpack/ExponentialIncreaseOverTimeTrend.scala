0d3e0e32d5cbc2a26db53a31d4b414455101d361
Grace Alberts <grace.alberts@revature.net>
2022-03-10 15:21:40 -0600

    Fixed error with Random Selection from merge

--- src/main/scala/producerpack/ExponentialIncreaseOverTimeTrend.scala
+++ src/main/scala/producerpack/ExponentialIncreaseOverTimeTrend.scala
@@ -3,6 +3,8 @@ package producerpack
 import org.apache.spark.sql.SparkSession
 import producerpack.DataCollection
 
+import scala.collection.mutable.ListBuffer
+
 
 object ExponentialIncreaseOverTimeTrend {
   System.setProperty("hadoop.home.dir", "c:/winutils")
@@ -35,5 +37,59 @@ object ExponentialIncreaseOverTimeTrend {
   def generateChangeCoefficient(ex:Double,baseInt:Int): Double ={
     //it will take
     ex*Math.pow(2,baseInt)
+  }
+
+  def increaseDate(date:ListBuffer[Int]):Unit={
+    //date will come in MM, DD, YYYY
+    //check specail months, 2 and 12. leap years arent considered
+    date(0) match {
+      case 2 =>
+        if(date(1) == 28) {
+          //roll month over, set day to one
+          date(0)+=1
+          date(1) = 1
+          return
+        }
+      case 12 =>
+        if (date(1) == 31) {
+          //roll month to 1, day to 1, year by 1
+          date(0) = 1
+          date(1) = 1
+          date(2) += 1
+          return
+        }
+    }
+    //now that specail months are out of the way...
+    if(date(0) > 7){
+      date(0)%2 match {
+        case 0 =>
+          if(date(1) == 30) {
+            //roll month and set day to 1
+            date(0) += 1
+            date(1) = 1
+          }
+        case 1 =>
+          if (date(1) == 31){
+            //roll month
+            date(0) += 1
+            date(1) = 1
+          }
+      }
+    } else {
+      date(0)%2 match {
+        case 0 =>
+          if(date(1) == 31) {
+            //roll month and set day to 1
+            date(0) += 1
+            date(1) = 1
+          }
+        case 1 =>
+          if (date(1) == 30){
+            //roll month
+            date(0) += 1
+            date(1) = 1
+          }
+      }
+    }
   }
 }
