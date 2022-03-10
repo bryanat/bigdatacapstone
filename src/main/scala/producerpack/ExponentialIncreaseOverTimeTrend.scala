2c8bb7d66d78cbe57ab205562074e7111f451f38
Grace Alberts <grace.alberts@revature.net>
2022-03-10 13:58:53 -0600

    Still fixing issues with merge

--- src/main/scala/producerpack/ExponentialIncreaseOverTimeTrend.scala
+++ src/main/scala/producerpack/ExponentialIncreaseOverTimeTrend.scala
@@ -14,7 +14,7 @@ object ExponentialIncreaseOverTimeTrend {
     .getOrCreate()
   spark.sparkContext.setLogLevel("ERROR")
   //we will used Camerons DataCollection object to get our data.
-  def createTrend(dataPoints:Int):Vector[String] = {
+  def createTrend(dataPoints:Int, pAmntVar:Int, baseAmount:Int, priceVar:Double):Vector[String] = {
     //I will try and
     var resultVector = Vector[String]()
     //get the data from object
@@ -24,15 +24,11 @@ object ExponentialIncreaseOverTimeTrend {
     val failReasons = collector.getfailReasonsList(spark)
     val websites = collector.getWebsiteList(spark)
     val paymentTypes = collector.getPaymentList(spark)
-    //This is the variance between the number of purchases a day, so they will be +/-10 purchances a day
-    val purchaseAmountVariance = 10
-    //base number of sales made per day
-    val basePurchasesPerDay = 30
-    //amount the price varies per purchase as a percentage
-    val priceVariance = 0.05
-    //finally, the step amount, which will be calculated. the max amount to change would be 10 times the start.
     val stepAmount:Double =  10/dataPoints
-    //i think i can finally get that
+    //i think i can finally get to doing the actual thing
+    for(i<-0 until dataPoints) {
+        //we will loop through until we have
+    }
 
     resultVector
   }
