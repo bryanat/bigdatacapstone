9d61f371b42063530162e794d57d4299a56f292e
bryanat <boyettbryan@hotmail.com>
2022-03-09 20:34:53 -0500

    Refactor code

--- src/main/scala/consumerpack/YashConsumer.scala
+++ src/main/scala/consumerpack/YashConsumer.scala
@@ -29,35 +29,54 @@ object YashConsumer {
         }
     }
 }
-*/
+ */
 
 object YashConsumer extends App {
-    System.setProperty("hadoop.home.dir", "c:/winutils")
-    val spark = SparkSession
-      .builder()
-      .appName("project1")
-      .config("spark.master", "local")
-      .getOrCreate()
+  System.setProperty("hadoop.home.dir", "c:/winutils")
+  val spark = SparkSession
+    .builder()
+    .appName("project1")
+    .config("spark.master", "local")
+    .getOrCreate()
 
-    val table = spark.read.option("header", "true").csv("dataset-online/samplecsv.csv")
-    //table.show();
-    table.createOrReplaceTempView("t1")
+  val table =
+    spark.read.option("header", "true").csv("dataset-online/samplecsv.csv")
+  // table.show();
+  table.createOrReplaceTempView("t1")
 
-    //failed transactions
-    spark.sql("select order_id, payment_txn_id, product_name, price, product_category, city, country from t1 where payment_txn_success = 'N'").show()
+  // failed transactions
+  spark
+    .sql(
+      "select order_id, payment_txn_id, product_name, price, product_category, city, country from t1 where payment_txn_success = 'N'"
+    )
+    .show()
 
-    //failed transactions by location
-    spark.sql("select order_id, payment_txn_id, product_name, price, city, country from t1 where payment_txn_success = 'N' order by city desc, country desc").show()
+  // failed transactions by location
+  spark
+    .sql(
+      "select order_id, payment_txn_id, product_name, price, city, country from t1 where payment_txn_success = 'N' order by city desc, country desc"
+    )
+    .show()
 
-    //failed transactions by price
-    spark.sql("select order_id, payment_txn_id, product_name, price from t1 where payment_txn_success = 'N' order by price desc")
+  // failed transactions by price
+  spark.sql(
+    "select order_id, payment_txn_id, product_name, price from t1 where payment_txn_success = 'N' order by price desc"
+  )
 
-    //failed transactions by category
-    spark.sql("select order_id, payment_txn_id, product_name, product_category from t1 where payment_txn_success = 'N' order by product_category desc").show()
+  // failed transactions by category
+  spark
+    .sql(
+      "select order_id, payment_txn_id, product_name, product_category from t1 where payment_txn_success = 'N' order by product_category desc"
+    )
+    .show()
 
-    //count of each failure reason -- CANT DO ON SAMPLE DATA WITHOUT COL failure_reason
-    //spark.sql("select failure_reason, count(payment_txn_id) as quantity from t1 where payment_txt_success = 'N' group by failure_reason").show()
+  // count of each failure reason -- CANT DO ON SAMPLE DATA WITHOUT COL failure_reason
+  // spark.sql("select failure_reason, count(payment_txn_id) as quantity from t1 where payment_txt_success = 'N' group by failure_reason").show()
 
-    //count of successful transactions vs failure transactions
-    spark.sql("select payment_txn_success, count(payment_txn_id) as quantity from t1 group by payment_txn_success").show()
+  // count of successful transactions vs failure transactions
+  spark
+    .sql(
+      "select payment_txn_success, count(payment_txn_id) as quantity from t1 group by payment_txn_success"
+    )
+    .show()
 }
