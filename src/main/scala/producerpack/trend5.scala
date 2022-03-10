119d0b89f4ce7ad560723618a716e50f4956a1b9
Betty Chou <phxchou@gmail.com>
2022-03-10 13:42:56 -0700

    Add my first trend

---
+++ src/main/scala/producerpack/trend5.scala
@@ -0,0 +1,16 @@
+package producerpack
+
+import org.apache.spark.sql.SparkSession
+
+import java.util.Random
+import scala.collection.mutable.ListBuffer
+
+// Trend Five will ______show a larger amount of online grocery orders from North America than any other country.
+// Our return string will be in the following format:
+// "order_id,customer_id,customer_name,product_id,product_name,product_category,payment_type,qty,price,datetime,
+// country,city,website,pay_id,success, timestamp"
+
+
+object trend5 {
+
+}
