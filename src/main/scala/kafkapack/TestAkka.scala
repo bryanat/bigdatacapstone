73b23d758bb0c9d7150f394d1947e8c5c7acee5c
HamingnottT <Jnottingham836@yahoo.com>
2022-03-11 09:55:12 -0600

    push Akka Sandbox

---
+++ src/main/scala/kafkapack/TestAkka.scala
@@ -0,0 +1,21 @@
+package kafkapack
+
+/* The purpose of this file is to practice and create a test Connection
+ * Using Akka. Once connection is established - code will be integrated in 
+ * KafkaAkka.scala */
+
+import akka.stream._
+import akka.stream.scaladsl._
+import akka.{ Done, NotUsed }
+import akka.actor.ActorSystem
+import akka.util.ByteString
+import scala.concurrent._
+import scala.concurrent.duration._
+import java.nio.file.Paths
+
+object TestAkka extends App {
+    implicit val system: ActorSystem = ActorSystem("QuickStart")
+//   Code here
+    val source: Source[Int, NotUsed] = Source(1 to 100)
+
+}
