daeba3a91e9b2d63b6067cce500c5000775ce415
yueqi <yueqi.peng@revature.net>
2022-03-12 11:42:31 -0600

    update consumer hive needs help on index out of bound

--- src/main/scala/kafkapack/HDFSGetHive.scala
+++ src/main/scala/kafkapack/HDFSGetHive.scala
@@ -4,14 +4,16 @@ import org.apache.hadoop.fs.{FileSystem, Path}
 import java.net.URI
 import scala.collection.immutable.Stream
 
-object HDFSGet {
+object HDFSGetHive {
    def getFromHDFS(): Unit = {
     val hdfs = FileSystem.get(new URI("hdfs://yourUrl:port/"), new Configuration()) 
     val path = new Path("/path/to/file/")
     val stream = hdfs.open(path)
-    def readLines = Stream.cons(stream.readLine, Stream.continually( stream.readLine))
+
+    
+    //def readLines = Stream.cons(stream.readLine, Stream.continually( stream.readLine))
 
     //This example checks line for null and prints every existing line consequentally
-    readLines.takeWhile(_ != null).foreach(line => println(line))
+    //readLines.takeWhile(_ != null).foreach(line => println(line))
    }
 }
