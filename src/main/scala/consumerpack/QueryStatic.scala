7a4405ac4cb4be3bde8c34a84d95f0adbe643e52
HamingnottT <Jnottingham836@yahoo.com>
2022-03-14 10:35:26 -0500

    Upload Query First Push

---
+++ src/main/scala/consumerpack/QueryStatic.scala
@@ -0,0 +1,40 @@
+package consumerpack
+
+import org.apache.spark._
+import org.apache.spark.sql._
+
+object QueryStatic {
+
+  def main(args: Array[String]): Unit = {
+
+  val warehouseLocation = "${system:user.dir}/spark-warehouse"
+    System.setProperty("hadoop.home.dir", "C:\\winutils")
+
+  val sparkConf = new SparkConf()
+      .set("spark.sql.warehouse.dir", warehouseLocation)
+      .set("spark.sql.catalogImplementation","hive")
+      .setMaster("local[*]")
+      .setAppName("p3")
+
+  val ssql = SparkSession
+      .builder
+      .config(sparkConf)
+      .config("spark.executor.memory", "48120M") 
+      .config("spark.sql.warehouse.dir", warehouseLocation)
+      .enableHiveSupport()
+      .getOrCreate()
+
+  // Drop the table if it already exists 
+    ssql.sql("DROP TABLE IF EXISTS hivetable")
+    // Create the table to store your streams 
+    ssql.sql("CREATE TABLE hivetable (order_id STRING, customer_id STRING, customer_name STRING, product_id STRING, product_name STRING, " +
+      "product_category STRING, payment_type STRING, qty STRING, price STRING, datetime STRING, country STRING, city STRING, " +
+      "ecommerce_website_name STRING, payment_txn_id STRING, payment_txn_success STRING, failure_reason STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE")
+
+    // val df_main = ssql.read.option("multiline","true").parquet("dataset-offline/")
+
+    ssql.sql("LOAD DATA LOCAL INPATH 'dataset-online/sample-of-final-data.csv' OVERWRITE INTO TABLE hivetable")
+    
+    ssql.sql("SELECT * FROM hivetable").show()
+  }
+}
