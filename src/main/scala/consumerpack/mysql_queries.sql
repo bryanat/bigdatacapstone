65c96f6f22af5b00f674fc6ab7b116f4f80284b5
BConover22 <98237598+BConover22@users.noreply.github.com>
2022-03-16 11:24:21 -0500

    Update mysql_queries.sql

--- src/main/scala/consumerpack/mysql_queries.sql
+++ src/main/scala/consumerpack/mysql_queries.sql
@@ -4,3 +4,9 @@ create or replace view ft_p as (select order_id, payment_txn_id, product_name, p
 create or replace view ft_c as (select order_id, payment_txn_id, product_name, product_category from hivetable where payment_txn_success = 'N' order by product_category desc);
 create or replace view fr_count as (select failure_reason, count(payment_txn_id) as quantity from hivetable where payment_txn_success = 'N' group by failure_reason);
 create or replace view sVf as (select payment_txn_success, count(payment_txn_id) as quantity from hivetable group by payment_txn_success);
+--Brandon Queries
+--Most Common payment Methods 
+df.groupBy("payment_type").count().orderBy(col("count")).withColumnRenamed("count", "payment_method").show(999)
+--Avg purchase cost per person, grouped by ecommerce site
+val df_price = df.withColumn("price", round(col("price"), 2))
+df_price.where(df("payment_txn_success") === "Y").groupBy("ecommerce_website_name").avg("price").withColumnRenamed("avg(price)", "cost_per_person").orderBy(col("cost_per_person")).show(999)
